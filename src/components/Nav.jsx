29649b377a51d720be160890380948b8f77f0182
johnson <kryzbone@gmail.com>
2020-06-12 22:30:22 +0000

    nav change

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Nav() {
     return (
         <div class="p-1">
-            <ul class="nav flex-lg-column flex-sm-row border-right border-bottom " >
+            {/* <ul class="nav flex-lg-column flex-sm-row border-right border-bottom " >
                 <li class="nav-item">
                     <a class="nav-link shop-list active" href="#">
                     <i class="fa fa-shopping-cart mr-2" aria-hidden="true"></i>
@@ -34,7 +34,29 @@ function Nav() {
                         Bags
                     </a>
                 </li>
-            </ul>
+            </ul> */}
+            <nav class="nav nav-pills flex-lg-column flex-sm-row">
+                <a class=" nav-link active" href="#">
+                    <i class="fa fa-shopping-cart mr-2" aria-hidden="true"></i>
+                    Clothes
+                </a>
+                <a class=" nav-link" href="#">
+                    <i class="fa fa-tag mr-2" aria-hidden="true"></i>
+                    Shoes
+                </a>
+                <a class=" nav-link" href="#">
+                    <i class="fa fa-graduation-cap mr-2" aria-hidden="true"></i>
+                    Cap
+                </a>
+                <a class=" nav-link" href="#">
+                    <i class="fa fa-google-wallet mr-2" aria-hidden="true"></i>
+                    Wallet
+                </a>
+                <a class=" nav-link" href="#">
+                    <i class="fa fa-shopping-bag mr-2" aria-hidden="true"></i>
+                    Bags
+                </a>
+            </nav>
         </div>
     )
 }
